4a44b10d7d4238ef5961e76aa46a559aba9b8453
Babsrm <babsruizm@gmail.com>
2022-03-08 21:15:23 +0100

    add testing function but still not working on event

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ let pieces = document.getElementsByClassName('pieces-js');
 let piecesSizeWidth = [134,192,134,163,134,163,134,192,134];
 let piecesSizeHeight = [163,134,163,134,192,134,163,134,163];
 
+//arrays con posiciones correctas de las piezas
+let origX = [200,304,466,200,333,437,200,304,466];
+let origY = [100,100,100,233,204,233,337,366,337];
+
 //variables para almacenar información de movimiento de piezas
 
 let elementSelect = 0;
@@ -15,6 +19,9 @@ let currentPosY = 0;
 //variable global que almacena todo el entorno variable gráfico
 
 let enviroment = document.getElementById('enviroment');
+
+//variable para cuando el puzzle ha sido resuelto bien y reproduzca sonido
+let win = document.getElementById('win');
 
 for (let i=0; i<pieces.length; i++){
     // le damos a cada pieza su tamaño
@@ -65,10 +72,11 @@ function moveElement(ev) {
   //deseleccionamos la pieza
   elementSelect.setAttribute("onmouseout","unselectPiece(ev)");
   elementSelect.setAttribute("onmouseup","unselectPiece(ev)");
-
+  magnet();
 }
 
 function unselectPiece(ev) {
+  testing();
 
   //eliminamos los elementos añadidos previamente, si procede
   if (elementSelect !=0){
@@ -93,3 +101,35 @@ function reorder (ev) {
 }
 
 //mejoramos la interacción con el usuario, dándole a las piezas la opción de imantarlas y que se junten fácilmente entre ellas
+
+function magnet () {
+  //recorremos todas las piezas. si la posición se encuentra en un radio de 15px de la posición correcta, la pieza será atraída a la posición buena
+  for (let i=0;i<pieces.length;i++) {
+    if (Math.abs(currentPosX-origX[i]) <15 && Math.abs(currentPosY-origY[i])<15)
+    elementSelect.setAttribute("x", origX[i]);
+    elementSelect.setAttribute("y", origY[i]);
+  }
+}
+
+//comprobamos que el puzzle ha sido formado correctamente
+
+function testing () {
+  //contador de piezas correctamente bien ubicadas
+let correctPieceLocation = 0;
+
+  //almacenamos en una variable todos los elementos con la clase parent
+  let parents = document.getElementsByClassName('parent');
+  for (let i=0;i<pieces.length;i++){
+    //obtenemos la posicion de cada elemento child de los elementos padres
+    let posX = parseFloat(parents[i].firstChild.getAttribute("x"));
+    let posY = parseFloat(parents[i].firstChild.getAttribute("y"));
+
+    //obtenemos el índice para poder hacer las comparaciones con las posiciones correctas
+    ide = parents[i].getAttribute("id");
+    if (origX[ide] == posX && origY[ide] == posY){
+      correctPieceLocation = correctPieceLocation +1;
+    }
+    if (correctPieceLocation == 9){
+      win.play();
+    }
+}}
